867a89da04f8404159dc80c48a6c1d9c48a813aa
ier <ivan.erakhtin@gmail.com>
2022-05-20 19:52:35 +0300

    Add unit test

--- test/walker/core_test.clj
+++ test/walker/core_test.clj
@@ -1,7 +1,25 @@
 (ns walker.core-test
-  (:require [clojure.test :refer :all]
-            [walker.core :refer :all]))
+  (:require [clojure.test :refer [deftest is]]
+            [walker.core :as w]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest main-test
+  (let [input (w/read-structure "resources/structure.edn")
+        expected ["root/Отчетность/По годам/2021/Q1.xls"
+                  "root/Отчетность/По годам/2021/Q2.xls"
+                  "root/Отчетность/По годам/2021/Q3.xls"
+                  "root/Отчетность/По годам/2021/Q4.xls"
+                  "root/Отчетность/По годам/Бюджет на 2021.xls"
+                  "root/Отчетность/По годам/2020/Q1.xls"
+                  "root/Отчетность/По годам/2020/Q2.xls"
+                  "root/Отчетность/По годам/2020/Q3.xls"
+                  "root/Отчетность/По годам/2020/Q4.xls"
+                  "root/Отчетность/Предварительный отчет Q1 2022.xls"
+                  "root/test.txt"
+                  "root/Документы/Служебная записка.doc"
+                  "root/Документы/Бюджеты/2021.xls"
+                  "root/Документы/Бюджеты/Промежуточные/Q1 2021.xls"
+                  "root/Документы/Бюджеты/Промежуточные/Q2 2021.xls"
+                  "root/Документы/Бюджеты/2022.xls"
+                  "root/temp"]
+        actual (w/m->v input)]
+    (is (= expected actual))))
